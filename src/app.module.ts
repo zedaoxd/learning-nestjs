4c455c81200306fbe9f44c965cee086fcb34e6de
Bruno Lessa <brunolessa@live.com>
2024-01-01 11:12:06 -0300

    Add TypeOrmModule configuration to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DbConfigService } from './config/db.config.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [
+    UsersModule,
+    ProductsModule,
+    TypeOrmModule.forRootAsync({
+      useClass: DbConfigService,
+      inject: [DbConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
